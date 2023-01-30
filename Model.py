32f26d3c5f0e3011c4b895092fc7f967626c2f45
Dev-Gaju <gajuahmd@gmail.com>
2023-01-30 16:25:12 +0600

    gunicorn update

--- Model.py
+++ Model.py
@@ -6,6 +6,8 @@ from nltk.stem.porter import PorterStemmer
 import json
 from torch.utils.data import Dataset
 
+# import
+# print (gunicorn.__version__)
 
 class NeuralNet(nn.Module):
     def __init__(self, input_size, hidden_size, num_classes):

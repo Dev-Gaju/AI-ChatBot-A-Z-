8fed82fda91fe9392f6b87ca015cc61da36cc3b3
Gaju Ahmed <50872508+Dev-Gaju@users.noreply.github.com>
2023-01-30 19:53:08 +0600

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import pdfkit
 from datetime import datetime
 from Model import NeuralNet, tokenize, bag_of_words
 
-# nltk.download('punkt')
+nltk.download('punkt')
 url = "model/intents.json"
 with open(url, 'r', encoding='utf-8') as f:
     intents = json.load(f)
